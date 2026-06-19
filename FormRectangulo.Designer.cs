f24e597016c4b557504076a73ca16908f24310ed
RafaelMarquez <rafamarquez0106@gmail.com>
2026-06-19 13:34:12 -0600

    Mejoras en UI y lógica de FormRectangulo
    Se ajustan tamaños de controles y fuentes para mejor legibilidad. Se agrega el botón "Calcular" con su evento, que calcula y muestra área y perímetro usando Clsrectangulo. Se reorganiza el groupbox de resultados y se declaran explícitamente los controles. Se inicializa Clsrectangulo en el constructor y se actualiza el archivo .resx con estructura estándar.

--- FormRectangulo.Designer.cs
+++ FormRectangulo.Designer.cs
@@ -27,38 +27,51 @@ namespace FigurasFlow
             this.lblAreaLabel = new System.Windows.Forms.Label();
             this.gbResultados.SuspendLayout();
             this.SuspendLayout();
-
+            // 
+            // lblBase
+            // 
             this.lblBase.AutoSize = true;
             this.lblBase.Location = new System.Drawing.Point(20, 22);
             this.lblBase.Name = "lblBase";
-            this.lblBase.Size = new System.Drawing.Size(34, 13);
+            this.lblBase.Size = new System.Drawing.Size(42, 16);
             this.lblBase.TabIndex = 0;
             this.lblBase.Text = "Base:";
-
+            // 
+            // txtBase
+            // 
             this.txtBase.Location = new System.Drawing.Point(80, 19);
             this.txtBase.Name = "txtBase";
-            this.txtBase.Size = new System.Drawing.Size(100, 20);
+            this.txtBase.Size = new System.Drawing.Size(100, 22);
             this.txtBase.TabIndex = 1;
-
+            // 
+            // lblAltura
+            // 
             this.lblAltura.AutoSize = true;
             this.lblAltura.Location = new System.Drawing.Point(20, 52);
             this.lblAltura.Name = "lblAltura";
-            this.lblAltura.Size = new System.Drawing.Size(37, 13);
+            this.lblAltura.Size = new System.Drawing.Size(44, 16);
             this.lblAltura.TabIndex = 2;
             this.lblAltura.Text = "Altura:";
-
+            // 
+            // txtAltura
+            // 
             this.txtAltura.Location = new System.Drawing.Point(80, 49);
             this.txtAltura.Name = "txtAltura";
-            this.txtAltura.Size = new System.Drawing.Size(100, 20);
+            this.txtAltura.Size = new System.Drawing.Size(100, 22);
             this.txtAltura.TabIndex = 3;
-
+            // 
+            // btnCalcular
+            // 
             this.btnCalcular.Location = new System.Drawing.Point(200, 33);
             this.btnCalcular.Name = "btnCalcular";
             this.btnCalcular.Size = new System.Drawing.Size(120, 23);
             this.btnCalcular.TabIndex = 4;
             this.btnCalcular.Text = "Calcular";
             this.btnCalcular.UseVisualStyleBackColor = true;
-
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // gbResultados
+            // 
             this.gbResultados.Controls.Add(this.lblPerimetro);
             this.gbResultados.Controls.Add(this.lblArea);
             this.gbResultados.Controls.Add(this.lblPerimetroLabel);
@@ -69,35 +82,46 @@ namespace FigurasFlow
             this.gbResultados.TabIndex = 5;
             this.gbResultados.TabStop = false;
             this.gbResultados.Text = "Resultados";
-
-            this.lblAreaLabel.AutoSize = true;
-            this.lblAreaLabel.Location = new System.Drawing.Point(20, 30);
-            this.lblAreaLabel.Name = "lblAreaLabel";
-            this.lblAreaLabel.Size = new System.Drawing.Size(32, 13);
-            this.lblAreaLabel.TabIndex = 0;
-            this.lblAreaLabel.Text = "Área:";
-
-            this.lblArea.AutoSize = true;
-            this.lblArea.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblArea.Location = new System.Drawing.Point(120, 30);
-            this.lblArea.Name = "lblArea";
-            this.lblArea.Size = new System.Drawing.Size(0, 13);
-            this.lblArea.TabIndex = 1;
-
-            this.lblPerimetroLabel.AutoSize = true;
-            this.lblPerimetroLabel.Location = new System.Drawing.Point(20, 60);
-            this.lblPerimetroLabel.Name = "lblPerimetroLabel";
-            this.lblPerimetroLabel.Size = new System.Drawing.Size(57, 13);
-            this.lblPerimetroLabel.TabIndex = 2;
-            this.lblPerimetroLabel.Text = "Perímetro:";
-
+            // 
+            // lblPerimetro
+            // 
             this.lblPerimetro.AutoSize = true;
             this.lblPerimetro.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblPerimetro.Location = new System.Drawing.Point(120, 60);
             this.lblPerimetro.Name = "lblPerimetro";
-            this.lblPerimetro.Size = new System.Drawing.Size(0, 13);
+            this.lblPerimetro.Size = new System.Drawing.Size(0, 17);
             this.lblPerimetro.TabIndex = 3;
-
+            // 
+            // lblArea
+            // 
+            this.lblArea.AutoSize = true;
+            this.lblArea.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblArea.Location = new System.Drawing.Point(120, 30);
+            this.lblArea.Name = "lblArea";
+            this.lblArea.Size = new System.Drawing.Size(0, 17);
+            this.lblArea.TabIndex = 1;
+            // 
+            // lblPerimetroLabel
+            // 
+            this.lblPerimetroLabel.AutoSize = true;
+            this.lblPerimetroLabel.Location = new System.Drawing.Point(20, 60);
+            this.lblPerimetroLabel.Name = "lblPerimetroLabel";
+            this.lblPerimetroLabel.Size = new System.Drawing.Size(68, 16);
+            this.lblPerimetroLabel.TabIndex = 2;
+            this.lblPerimetroLabel.Text = "Perímetro:";
+            // 
+            // lblAreaLabel
+            // 
+            this.lblAreaLabel.AutoSize = true;
+            this.lblAreaLabel.Location = new System.Drawing.Point(20, 30);
+            this.lblAreaLabel.Name = "lblAreaLabel";
+            this.lblAreaLabel.Size = new System.Drawing.Size(39, 16);
+            this.lblAreaLabel.TabIndex = 0;
+            this.lblAreaLabel.Text = "Área:";
+            // 
+            // FormRectangulo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(344, 211);
             this.Controls.Add(this.gbResultados);
@@ -115,6 +139,7 @@ namespace FigurasFlow
             this.gbResultados.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
 
         private System.Windows.Forms.Label lblBase;
