d5c1716c06cd53d7ce4c4eaee8ead9cf1bda3c45
RafaelMarquez <rafamarquez0106@gmail.com>
2026-06-19 13:24:51 -0600

    Añadir clase Clsrectangulo para área y perímetro
    Se crea el archivo Clsrectangulo.cs con la clase Clsrectangulo, que hereda de Clsfiguras e implementa las propiedades ladoMayor y ladoMenor, así como los métodos para calcular el área y el perímetro del rectángulo.

---
+++ Clsrectangulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigurasFlow
+{
+    internal class Clsrectangulo : Clsfiguras
+    {
+        public double ladoMayor { get; set; }
+        public double ladoMenor { get; set; }
+        public override double CalcularArea()
+        {
+            return ladoMayor * ladoMenor;
+        }
+
+        public override double CalcularPerimetro()
+        {
+           return 2 * (ladoMayor + ladoMenor);
+        }
+    }
+}
