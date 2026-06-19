210e65be088d5880b14b285505471f9f353c27cd
JNicacio <juanhernandezalv11@gmail.com>
2026-06-19 09:20:02 -0600

    se genero la clse uadrado heredada de la cls figura

---
+++ Cuadrado.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigurasFlow
+{
+    internal class Cuadrado : Clsfiguras
+
+    {
+        public double lado {  get; set; }
+         
+
+        public override double CalcularArea()
+        {
+            return lado * lado;
+
+        }
+
+        public override double CalcularPerimetro()
+        {
+            return lado * 4;
+        }
+    }
+}
